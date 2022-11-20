33444ab0d57a2c54c63d8666772e723ae1a45cb8
Kyle Gibbs <kyletgibbs@gmail.com>
2022-11-20 15:18:37 -0500

    EnemyManager now keeps track of current wave and can transition from one wave to another once all enemies in the current wave are spawned.

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -10,7 +10,9 @@ public class EnemyManager : MonoBehaviour
     public WaveCollection waveCollection;
     public Transform spawnPoint;
     public int currentWave = 0;
-
+    public int enemiesSpawnedThisWave = 0;
+    public int totalEnemiesInWave = 0;
+    public bool waveRunning = false;
     private readonly string WavesFilePath = "Assets\\Waves.xml";
 
     // Start is called before the first frame update
@@ -18,12 +20,25 @@ public class EnemyManager : MonoBehaviour
     {
         ImportWaves();
         BeginWave(currentWave);
-        //StartCoroutine(SpawnEnemies());
     }
 
     // Update is called once per frame
     private void Update()
     {
+       if(waveRunning)
+       {
+            if(enemiesSpawnedThisWave < totalEnemiesInWave)
+            {
+                return;
+            }
+            else
+            {
+                Debug.Log("Wave Complete");
+                waveRunning = false;
+                currentWave++;
+                BeginWave(currentWave);
+            }
+       }
     }
 
     private void ImportWaves()
@@ -43,20 +58,33 @@ public class EnemyManager : MonoBehaviour
 
     private void BeginWave(int waveNumber)
     {
+        if(currentWave >= waveCollection.Waves.Count)
+        {
+            Debug.Log("All waves complete");
+            return;
+        }
+
+        enemiesSpawnedThisWave = 0;
+        totalEnemiesInWave = 0;
+        waveRunning = true;
+
+        Debug.Log($"Starting wave {waveNumber}");
         foreach (EnemyPack ep in waveCollection.Waves[waveNumber].EnemyPacks)
         {
+            totalEnemiesInWave += ep.EnemyCount;
             StartCoroutine(SpawnEnemies(ep));
         }
     }
 
     private IEnumerator SpawnEnemies(EnemyPack ep)
     {
-        Debug.Log("SpawnEnemies");
+        // Debug.Log("SpawnEnemies");
         if (ep.Type == 1)
         {
             for (int i = 0; i < ep.EnemyCount; i++)
             {
                 var enemy = Instantiate(Enemy1Prefab);
+                enemiesSpawnedThisWave++;
                 enemy.transform.position = spawnPoint.position + new Vector3(0, 1, 0);
                 yield return new WaitForSeconds(2);
             }
@@ -66,6 +94,7 @@ public class EnemyManager : MonoBehaviour
             for (int i = 0; i < ep.EnemyCount; i++)
             {
                 var enemy = Instantiate(Enemy2Prefab);
+                enemiesSpawnedThisWave++;
                 enemy.transform.position = spawnPoint.position;
                 yield return new WaitForSeconds(2);
             }
